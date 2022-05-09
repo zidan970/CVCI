9c13a274c34e2c61d144bc57dd4ea4c4b0ca3b77
zidan <amdzidan70@gmail.com>
2022-05-09 16:13:00 +0700

    upload

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,303 @@
+<html>
+<head>
+<!--Element-elemen tag <head> tulis disini-->
+	<title>WEBSITE PORTOFOLIO</title>
+	<link rel="stylesheet" href="layout.css" />
+	<style type="text/css">
+		body 
+		{
+    		width: 960px;
+    		margin: 30px auto;
+		}
+
+		main 
+		{
+    		display: flex;
+		}
+
+		.padding_this
+		{
+    		padding: 15px;
+		}
+
+		.logo,
+		.konten1,
+		.sidebar1,
+		.konten2,
+		.sidebar2,
+		.konten3,
+		.sidebar3,
+		.konten4,
+		.sidebar4,
+		.footer 
+		{
+    		margin-bottom: 5px;
+    		padding: 15px 0;
+		}
+
+		.logo 
+		{
+    		width: 100%;
+    		height: auto;
+    		background: #37538f;
+    		text-align: center;
+    		color: #fff;
+		}
+
+		.konten1 
+		{
+    		width: 70%;
+    		background: #bbdcf1;
+		}
+
+		.sidebar1 
+		{
+    		width: 30%;
+    		background: #7490b9;
+		    text-align: center;
+		}
+
+		.konten2 
+		{
+    		width: 70%;
+    		background: #bbdcf1;
+		}
+
+		.sidebar2 
+		{
+    		width: 30%;
+    		background: #7490b9;
+    		text-align: center;
+		}
+
+		.konten3 
+		{
+    		width: 70%;
+    		background: #bbdcf1;
+		}
+
+		.sidebar3 
+		{
+    		width: 30%;
+    		background: #7490b9;
+    		text-align: center;
+		}
+
+		.konten4 
+		{
+    		width: 70%;
+    		background: #bbdcf1;
+		}
+
+		.sidebar4 
+		{
+    		width: 30%;
+    		background: #7490b9;
+    		text-align: center;
+		}
+
+		.footer 
+		{
+    		width: 100%;
+    		background: #37538f;
+    		text-align: center;
+		}
+	</style>
+</head>
+
+<body>
+<!--Element-elemen tag <body> tulis disini-->
+
+<header> <!--Section HEADER-->
+	<div class="logo">
+		<h1><i>Curriculum Vitæ</i></h1>
+	</div>
+</header>
+
+<main> <!--Section IDENTITAS-->
+	<div class="konten1">
+		<div class="padding_this">
+			<p>	
+				<table width="500" align="center">
+					<tr>
+						<td>Nama </td>
+						<td> : A. Zidan Abdillah Majid </td>
+					</tr>
+					<tr>
+						<td>Tempat, tanggal lahir </td>
+						<td> : Malang, 12 Maret 2001 </td>
+					</tr>
+					<tr>
+						<td>Jenis Kelamin   </td>
+						<td> : Laki-laki </td>
+					</tr>
+					<tr>
+						<td>Agama </td>
+						<td> : Islam </td>
+					</tr>
+					<tr>
+						<td>Alamat </td>
+						<td> : Perumahan Sumber Taman Indah Blok C No. 15</td>
+					</tr>
+					<tr>
+						<td>Email </td>
+						<td> : <a href="https://mail.google.com/">amdzidan970@gmail.com</a></td>
+					</tr>
+					<tr>
+						<td>Instagram </td>
+						<td> : <a href="https://www.instagram.com/a_zidann/">a_zidann</a></td>
+					</tr>
+					<tr>
+						<td>Twitter </td>
+						<td> : <a href="https://twitter.com/amdzidan/">amdzidan</a></td>
+					</tr>
+				</table>
+			</p>
+		</div>
+	</div>
+	<div class="sidebar1">
+		<div class="padding_this">
+			<h4>
+				<br><br><br><br>
+				IDENTITAS
+			</h4>
+		</div>
+	</div>
+</main>
+
+<main> <!--Section PENDIDIKAN-->
+	<div class="konten2">
+		<div class="padding_this">
+			<p>
+				<table width="500" align="center">
+					<tr>
+						<td>SDN SUMBERTAMAN 2</td>
+						<td>2007-2013</td>
+					</tr>
+					<tr>
+						<td>SMP NEGERI 5 PROBOLINGGO</td>
+						<td>2013-2016</td>
+					</tr>
+					<tr>
+						<td>SMA TARUNA DRA. ZULAEHA</td>
+						<td>2016-2019</td>
+					</tr>
+					<tr>
+						<td>INSTITUT TEKNOLOGI SEPULUH NOPEMBER</td>
+						<td>2019-SEKARANG</td>
+					</tr>
+				</table>
+			</p>
+		</div>
+	</div>
+	<div class="sidebar2">
+		<div class="padding_this">
+			<h4>
+				<br><br>
+				PENDIDIKAN
+			</h4>
+		</div>
+	</div>
+</main>
+
+<main> <!--Section ORGANISASI & KEPANITIAAN-->
+	<div class="konten3">
+		<div class="padding_this">
+			<p>
+				<table width="500" align="center">
+					<tr>
+						<td>Staf Departemen Hubungan Dalam UKM Rebana ITS</td>
+						<td>2020</td>
+					</tr>
+					<tr>
+						<td>Staf Departemen Syiar LDJ Keluarga Muslim Informatika (KMI) ITS</td>
+						<td>2020</td>
+					</tr>
+					<tr>
+						<td>Wakil Kepala Departemen Hubungan Dalam UKM Rebana ITS</td>
+						<td>2021</td>
+					</tr>
+					<tr>
+						<td>Sie Desain dan Dokumentasi (Desdok) dalam Kegiatan Lomba Cover
+							Sholawat Sepuluh Nopember Se-Nasional “Senandung Dinamika
+							Rebana (SDR)” UKM Rebana ITS
+						   </td>
+						<td>2020</td>
+					</tr>
+					<tr>
+						<td>Liaison Officer Kegiatan Pelatihan Spiritual dan Kebangsaan ITS</td>
+						<td>2021</td>
+					</tr>
+				</table>
+			</p>
+		</div>
+	</div>
+	<div class="sidebar3">
+		<div class="padding_this">
+			<h4>
+				<br><br><br>
+				ORGANISASI & KEPANITIAAN
+			</h4>
+		</div>
+	</div>
+</main>
+
+<main> <!--Section PELATIHAN-->
+	<div class="konten4">
+		<div class="padding_this">
+			<p>
+				<table width="500" align="center">
+					<tr>
+						<td>LKMW-TD</td>
+						<td>2019</td>
+					</tr>
+					<tr>
+						<td>Pelatihan Karya Tulis Ilmiah (PKTI)</td>
+						<td>2019</td>
+					</tr>
+					<tr>
+						<td>LKMM PRA-TD</td>
+						<td>2019</td>
+					</tr>
+					<tr>
+						<td>Diklat Anggota UKM Rebana ITS</td>
+						<td>2019</td>
+					</tr>
+					<tr>
+						<td>Staff Training UKM Rebana ITS</td>
+						<td>2019</td>
+					</tr>
+					<tr>
+						<td>Regenerasi Manajemen Organisasi (REMO) UKM Rebana ITS</td>
+						<td>2020</td>
+					</tr>
+					<tr>
+						<td>Pelatihan Artificial Intelligence Teknik Mesin</td>
+						<td>2021</td>
+					</tr>
+					<tr>
+						<td>Training Melukis Slide dengan PowerPoint</td>
+						<td>2021</td>
+					</tr>
+				</table>
+			</p>
+		</div>
+	</div>
+	<div class="sidebar4">
+		<div class="padding_this">
+			<h4>
+				<br><br><br><br>
+				PELATIHAN
+			</h4>
+		</div>
+	</div>
+</main>
+
+<footer> <!--Section FOOTER-->
+	<div class="footer">
+		<small><i>every artist was the first an amateur</i></small>
+	</div>
+</footer>
+
+</body>
+</html>
